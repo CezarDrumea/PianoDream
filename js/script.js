8f22cfa5a97bbe7ce60cfec77000fa074c73c926
Cezar <drumea.cezar@gmail.com>
2023-02-04 01:37:38 +0200

    add local storage for theme switcher

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 $(document).ready(function() {
+  if (localStorage.getItem("inner") === null) {
+    $(".color-mode-inner").removeClass("active");
+  } else {
+    $(".color-mode-inner").addClass("active");
+  }
+  $("body").useItemsLocalStorage("body-dark", "body-light", "body");
+  $("nav, .explore").useItemsLocalStorage("dark-blur", "light-blur", "blur")
+  $(".languages, .piano-dream").useItemsLocalStorage("dark-text", "light-text", "text");
+  $(".logo-btn").useItemsLocalStorage("logo-dark", "logo-light", "logo");
+  $(".facebook-btn").useItemsLocalStorage("fb-dark", "fb-light", "fb");
+  $(".instagram-btn").useItemsLocalStorage("ig-dark", "ig-light", "ig");
+  $(".more-btn").useItemsLocalStorage("more-dark", "more-light", "more");
+  $(".color-mode-btn").useItemsLocalStorage("color-dark", "color-light", "color");
+  $(".color-mode-inner").useItemsLocalStorage("inner-dark", "inner-light", "inner");
+  $(".nav-elements").useItemsLocalStorage("nav-el-dark", "nav-el-light", "nav-elements");
+  $(".bar").useItemsLocalStorage("bar-dark", "bar-light", "bar");
+  if (localStorage.getItem("meta-logo") !== null) {
+    $(".meta-logo").attr("href", localStorage.getItem("meta-logo"))
+  }
+
   $(".color-mode-btn").click(function() {
     $(".color-mode-inner").toggleClass("active");
-    $("body").toggleClass("body-dark").toggleClass("body-light");
-    $("nav, .explore").toggleClass("dark-blur").toggleClass("light-blur");
-    $(".languages, .piano-dream").toggleClass("dark-text").toggleClass("light-text");
-    $(".logo-btn").toggleClass("logo-dark").toggleClass("logo-light");
-    $(".facebook-btn").toggleClass("fb-dark").toggleClass("fb-light");
-    $(".instagram-btn").toggleClass("ig-dark").toggleClass("ig-light");
-    $(".more-btn").toggleClass("more-dark").toggleClass("more-light");
-    $(".color-mode-btn").toggleClass("color-dark").toggleClass("color-light");
-    $(".color-mode-inner").toggleClass("inner-dark").toggleClass("inner-light");
-    $(".nav-elements").toggleClass("nav-el-dark").toggleClass("nav-el-light");
-    $(".bar").toggleClass("bar-dark").toggleClass("bar-light");
-    $(".title-logo").toggleAttrVal("href", "img/logo/pianodream-logo-white.png", "img/logo/pianodream-logo-black.png");
+    if ($(".color-mode-inner").hasClass("active")) {
+      localStorage.setItem("inner", "active");
+    } else {
+      localStorage.setItem("inner", null);
+    }
+    $("body").toggleDarkLight("body-dark", "body-light", "body");
+    $("nav, .explore").toggleDarkLight("dark-blur", "light-blur", "blur")
+    $(".languages, .piano-dream").toggleDarkLight("dark-text", "light-text", "text");
+    $(".logo-btn").toggleDarkLight("logo-dark", "logo-light", "logo");
+    $(".facebook-btn").toggleDarkLight("fb-dark", "fb-light", "fb");
+    $(".instagram-btn").toggleDarkLight("ig-dark", "ig-light", "ig");
+    $(".more-btn").toggleDarkLight("more-dark", "more-light", "more");
+    $(".color-mode-btn").toggleDarkLight("color-dark", "color-light", "color");
+    $(".color-mode-inner").toggleDarkLight("inner-dark", "inner-light", "inner");
+    $(".nav-elements").toggleDarkLight("nav-el-dark", "nav-el-light", "nav-elements");
+    $(".bar").toggleDarkLight("bar-dark", "bar-light", "bar");
+    $(".meta-logo").toggleAttrVal("href", "img/logo/pianodream-logo-white.png", "img/logo/pianodream-logo-black.png", "meta-logo");
   });
 
   $(".hamburger").click(function () {
@@ -26,14 +51,34 @@ $(document).ready(function() {
   })
 });
 
+$.fn.toggleDarkLight = function (class1, class2, keyname) {
+    if ($(this).hasClass(class1)) {
+      localStorage.setItem(keyname, class2);
+    } else {
+      localStorage.setItem(keyname, class1);
+    }
+    return $(this).toggleClass(class1).toggleClass(class2);
+  }
 
+$.fn.useItemsLocalStorage = function (class1, class2, keyname) {
+  if (localStorage.getItem(keyname) !== null) {
+    if ($(this).hasClass(class1)) {
+      $(this).removeClass(class1);
+    } else {
+      $(this).removeClass(class2);
+    }
+    return $(this).addClass(localStorage.getItem(keyname));
+  }
+}
 
-$.fn.toggleAttrVal = function (attr, val1, val2) {
+$.fn.toggleAttrVal = function (attr, val1, val2, keyname) {
   const test = $(this).attr(attr);
   if (test === val1) {
+    localStorage.setItem(keyname, val2);
     return $(this).attr(attr, val2);
   }
   if (test === val2) {
+    localStorage.setItem(keyname, val1);
     return $(this).attr(attr, val1);
   }
 };
