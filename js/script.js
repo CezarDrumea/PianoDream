162e75e8e73bfbe870df9339b3f3458c9dd73593
Cezar <drumea.cezar@gmail.com>
2023-02-05 17:55:32 +0200

    add form inputs

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
     $(".form").toggleBtwn2Classes("form-dark", "form-light");
   });
 
-  $(".hamburger").click(function () {
+  $(".hamburger, .mobile").click(function () {
     $(".mobile").toggle(600);
   })
 
