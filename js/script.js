7e455e1aa6d85e3e2ce620ca2e871c9f7b0e4cf8
Cezar <drumea.cezar@gmail.com>
2023-02-11 16:55:13 +0200

    minor change in font size and weight

--- js/script.js
+++ js/script.js
@@ -83,45 +83,6 @@ $(document).ready(function () {
       $(".mobile").css("display", "none");
     }
   });
-
-  const form = $(".form-text");
-  const result = $(".form-result");
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-    result.innerHTML = "Se procesează...";
-
-    fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: json,
-    })
-      .then(async (response) => {
-        let json = await response.json();
-        if (response.status == 200) {
-          result.innerHTML = json.message;
-        } else {
-          console.log(response);
-          result.innerHTML = json.message;
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        result.innerHTML = "Nu am putut procesa formularul...";
-      })
-      .then(function () {
-        form.reset();
-        setTimeout(() => {
-          result.style.display = "none";
-        }, 3000);
-      });
-  });
 });
 
 $.fn.toggleBtwn2Classes = function (class1, class2) {
